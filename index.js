c0524849d6c576652ad145f6d09cfd8ed8d0d75a
adhillon0817 <avleendhillon@gmail.com>
2022-11-09 12:57:10 -0800

    added pseduo code

---
+++ index.js
@@ -0,0 +1,49 @@
+// require 'inquirer'
+
+
+
+// Create an empty array list to store employee objects outside of array.
+
+
+
+// Gather manager data
+
+    //inquirer.prompt()
+
+    //THEN Build a manager object
+
+
+
+// Gather Engineer data
+
+    //inquirer.prompt()
+
+    //THEN Build a Engineer object
+
+
+
+// Gather Intern data
+
+    //inquirer.prompt()
+
+    //THEN Build a Engineer object
+
+
+
+// Ask for which team member they want to add or are they done?
+
+        //inquirer.prompt()
+
+        // THEN decide which function to call (done, engineer, etc.)
+
+
+    
+// Generate the HTML 
+
+
+
+// Write the html file
+
+
+
+        
