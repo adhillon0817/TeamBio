e4d31488fa26b1d60f288adc6fd75d8976015b3f
adhillon0817 <avleendhillon@gmail.com>
2022-11-09 23:17:54 -0800

    added some outline for inquirer

--- index.js
+++ index.js
@@ -6,17 +6,39 @@ const inquirer = require('inquirer');
 
 const arr = []
 
-// Gather manager data
+
+
+//6:16 Day 1
+// Gather manager data 
 
     //inquirer.prompt()
 
     inquirer
-        .createPromptModule([
-            type: "input", 
-            message: ""
+        .prompt([
+            {
+            type: "list", 
+            message: "Which type of team member would you like to add?",
+            name: "type",
+            choices: [
+                {
+                    name: "Manager",
+                    value: "M"
+                }, 
+                {
+                    name: "Engineer",
+                    value:"E"
+                },
+                {
+                    name: "Intern",
+                    value:"I"
+                },
+            ]
+            }
         ])
-
-    //THEN Build a manager object
+        .then((answers) => {
+            console.log(answers);
+        });
+        
 
 
 
