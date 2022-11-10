84ec6ab563f8d2e46ea08b307a977ecdb91de9bd
adhillon0817 <avleendhillon@gmail.com>
2022-11-10 00:24:24 -0800

    added async and await

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // require 'inquirer'
 const inquirer = require('inquirer');
+const fs = require("fs");
+const utils = require("util");
 
-
+const writeFile = utils.promisify(fs.writeFile);
 // Create an empty array list to store employee objects outside of array.
 
 const arr = []
@@ -13,34 +15,103 @@ const arr = []
 
     //inquirer.prompt()
 
-    inquirer
-        .prompt([
-            {
-            type: "list", 
-            message: "Which type of team member would you like to add?",
-            name: "type",
-            choices: [
-                {
-                    name: "Manager",
-                    value: "M"
-                }, 
-                {
-                    name: "Engineer",
-                    value:"E"
-                },
-                {
-                    name: "Intern",
-                    value:"I"
-                },
-            ]
+   async function addEmployee() {
+    try {
+        const answers = await inquirer
+            inquirer
+                .prompt([
+                    {
+                        type: "list", 
+                        message: "Which type of team member would you like to add?",
+                        name: "type",
+                        choices: [
+                            {
+                                name: "Manager",
+                                value: "M"
+                            }, 
+                            {
+                                name: "Engineer",
+                                value:"E"
+                            },
+                            {
+                                name: "Intern",
+                                value:"I"
+                            },
+                            {
+                                name: "All Done Adding Employees!",
+                                value:"D"
+                            },
+                        ]
+                    }
+                ])
+                
+                    if(answers.type === "Manager") {
+                        addManager();
+                    }
+
+                    if(answers.type === "Engineer") {
+                        addEngineer();
+                    }
+
+                    if(answers.type === "Intern") {
+                        addIntern();
+                    } else {
+                        try {
+                        // answers.type === "All Done Adding Employees!"
+                            await writeFile("team.html", "Team Profile");
+                            console.log("Employees Added!");
+                        } catch(err) {
+                            console.error(err)
+                        }
+                } 
+            }catch(err) {
+                console.log(err);
             }
-        ])
-        .then((answers) => {
-            console.log(answers);
-        });
         
+}
+            
+
+    async function addManager() {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is the team manager's name?",
+                    name: "Manager",
+                }
+            ]);
+        
+        addEmployee();
+    }
 
 
+    async function addEngineer() {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is the team engineer's name?",
+                    name: "Engineer",
+                }
+            ]);
+        
+        addEmployee();
+    }
+
+    
+    async function addIntern() {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is the team intern's name?",
+                    name: "Intern",
+                }
+            ]);
+        
+        addEmployee();
+    }
+   
 
 // Gather Engineer data
 
