0907db13be12a9ea0ba1781360f2f7697345ff49
adhillon0817 <avleendhillon@gmail.com>
2022-11-12 22:55:43 -0800

    added test for employee

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -2,9 +2,11 @@ const Employee = require('.../lib/Employee.js');
 
 describe("Employee", () => {
     it("can create employee", () => {
-        const e = new Employee();
-        expect(typeof(e)).toBe("object";)
+        const newEmployee = new Employee('Avleen', 17, 'avleendhillon@gmail.com');
+
+        expect(newEmployee.name).toEqual(expect.any(String));
+        expect(newEmployee.id).toEqual(expect.any(Number));
+        expect(newEmployee.email).toEqual(expect.any(Number));
     });
 
-    it()
 })
