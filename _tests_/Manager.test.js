d712f81981209fc80c947544682af1abc0c52923
adhillon0817 <avleendhillon@gmail.com>
2022-11-12 23:39:19 -0800

    added manager test

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,1 +1,11 @@
- 
+ const Manager = require('../lib/Manager');
+
+ test('employee as manager',() => {
+    const newManager = new Manager('Avleen', 17, 'avleendhillon@gmail.com', 25);
+    expect(newManager.roomNumber).toEqual(expect.any(Number));
+});
+
+test('employee role', () => {
+    const newManager = new Manager('Avleen', 17, 'avleendhillon@gmail.com', 25);
+    expect(newEngineer.getRole()).toEqual("Manager");
+});
