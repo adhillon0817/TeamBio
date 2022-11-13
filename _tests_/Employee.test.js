766e53c01d3e2eef73595dfe731cb7ba76575e5a
adhillon0817 <avleendhillon@gmail.com>
2022-11-12 22:41:45 -0800

    added decribe function for employee.test

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,10 @@
+const Employee = require('.../lib/Employee.js');
+
+describe("Employee", () => {
+    it("can create employee", () => {
+        const e = new Employee();
+        expect(typeof(e)).toBe("object";)
+    });
+
+    it()
+})
