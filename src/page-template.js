8b5f62fe520f2765176e928085ba9eac2812cbd4
adhillon0817 <avleendhillon@gmail.com>
2022-11-21 18:02:43 -0800

    added to html page

--- src/page-template.js
+++ src/page-template.js
@@ -5,13 +5,13 @@ const generateEngineer = function (engineer) {
       <div class="col-4 mt-4">
                       <div class="card h-100">
                           <div class="card-title">
-                              <h2>${engineer.name}</h2>
+                              <h2>${engineer.getName()}</h2>
                               <h3>Engineer</h3>
                           </div>
                           <div class="card-body">
-                              <p class="id">${engineer.id}</p>
-                              <p class="email">email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
-                              <p class="gitHub">gitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+                              <p class="id">${engineer.getId()}</p>
+                              <p class="email">email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></p>
+                              <p class="gitHub">gitHub: <a href="https://github.com/${engineer.getGitHub()}">${engineer.getGitHub()}</a></p>
                           </div>
                       </div>
                   </div>
@@ -23,13 +23,13 @@ const generateEngineer = function (engineer) {
       <div class="col-4 mt-4">
                       <div class="card h-100">
                           <div class="card-title">
-                              <h2>${intern.name}</h2>
+                              <h2>${intern.getName()}</h2>
                               <h3>Intern</h3>
                           </div>
                           <div class="card-body">
-                              <p class="id">id:${intern.id}</p>
-                              <p class="email">email: <a href="mailto:${intern.email}">${intern.email}</a></p>
-                              <p class="school">School: ${intern.school}</p>
+                              <p class="id">id:${intern.getId()}</p>
+                              <p class="email">email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></p>
+                              <p class="school">School: ${intern.getSchool()}</p>
                           </div>
                       </div>
                   </div>
@@ -41,51 +41,24 @@ const generateEngineer = function (engineer) {
       <div class="col-4 mt-4">
                       <div class="card h-100">
                           <div class="card-title">
-                              <h2>${manager.name}</h2>
+                              <h2>${manager.getName()}</h2>
                               <h3>Manager</h3>
                           </div>
                           <div class="card-body">
-                              <p class="id">id: ${manager.id}</p>
-                              <p class="email">email: <a href="mailto:${manager.email}">${manager.email}</a></p>
-                              <p class="officeNumber">Office: ${manager.officeNumber}</p>
+                              <p class="id">id: ${manager.getId()}</p>
+                              <p class="email">email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></p>
+                              <p class="officeNumber">Office: ${manager.getOfficeNumber()}</p>
                           </div>
                       </div>
                   </div>
       `;
   };
 
-  const generateTeamProfile = function (teamCards) {
-    return `
-        <!DOCTYPE html>
-      <html lang="en">
-      <head>
-          <meta charset="UTF-8">
-          <meta http-equiv="X-UA-Compatible" content="IE=edge">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <meta http-equiv=""X-UA-Compatible" content="ie=edge" />
-          <title>Team Profile</title>
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-          <link rel="stylesheet" href="style.css">
-      </head>
-      
-      <body>
-        <header>
-            <nav class="navbar" style="background-color: #3B71CA;">
-                <div class="container-fluid">
-                <section class="navbar-brand mb-0 h1 w-100 text-center">My Team</section>
-            </div>
-            </nav>
-        </header>
-          <main>
-              <div class="container">
-                  <div class = "row justify-center align-center" id="position-card">
-                  ${teamCards}
-                  </div>
-              </div>
-          </main>
-        </body>
-      `;
-  };
+  // const generateTeamProfile = function (teamCards) {
+  //   return `
+        
+  //     `;
+  // };
 
   
  
@@ -101,6 +74,40 @@ const generateEngineer = function (engineer) {
     //creating manager
     pageArray.push(data.filter(employee => employee.getRole() === "Manager").map(employee => generateManager(manager)));
 
+retun(pageArray.join(""));
 
-}
-  module.exports = pageTemplate;
+};
+
+module.exports = team => {
+  return`
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv=""X-UA-Compatible" content="ie=edge" />
+      <title>Team Profile</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+      <link rel="stylesheet" href="style.css">
+  </head>
+  
+  <body>
+    <header>
+        <nav class="navbar" style="background-color: #3B71CA;">
+            <div class="container-fluid">
+            <section class="navbar-brand mb-0 h1 w-100 text-center">My Team</section>
+        </div>
+        </nav>
+    </header>
+      <main>
+          <div class="container">
+              <div class = "row justify-center align-center" id="position-card">
+              ${pageTemplate(team)}
+              </div>
+          </div>
+      </main>
+    </body>
+</html>
+  `
+};
